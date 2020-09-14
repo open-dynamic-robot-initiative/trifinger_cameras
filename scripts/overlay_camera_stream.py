3811d0f1792c9135eea428df1d7e0166f5e5873b
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-09-14 14:15:37 +0200

    Script to overlay Pylon stream with static image
    This can be used to align the camera of one platform with an image taken
    on another platform, such that their position is the same.

---
+++ scripts/overlay_camera_stream.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+"""
+"""
+import argparse
+import cv2
+
+import trifinger_cameras
+from trifinger_cameras import utils
+
+
+def main():
+    argparser = argparse.ArgumentParser(description=__doc__)
+    argparser.add_argument(
+        "--camera-id",
+        "-c",
+        required=True,
+        help="Camera name",
+    )
+    argparser.add_argument("--alpha", "-a", type=float, default=0.4)
+    argparser.add_argument(
+        "overlay_image",
+        type=str,
+    )
+    args = argparser.parse_args()
+
+    alpha = args.alpha
+    overlay_image = cv2.imread(args.overlay_image)
+    # use red channel only, so it is easy to distinguish from the actual image
+    overlay_image[:, :, 0] = 0
+    overlay_image[:, :, 1] = 0
+
+    camera_data = trifinger_cameras.camera.SingleProcessData()
+    camera_driver = trifinger_cameras.camera.PylonDriver(
+        args.camera_id, downsample_images=False)
+    camera_backend = trifinger_cameras.camera.Backend(  # noqa
+        camera_driver, camera_data
+    )
+    camera_frontend = trifinger_cameras.camera.Frontend(camera_data)
+
+    while True:
+        observation = camera_frontend.get_latest_observation()
+        image = utils.convert_image(observation.image)
+
+        image = cv2.addWeighted(image, 1 - alpha, overlay_image, alpha, 0)
+
+        cv2.imshow("Camera", image)
+        # stop if either "q" or ESC is pressed
+        if cv2.waitKey(10) in [ord("q"), 27]:  # 27 = ESC
+            break
+
+
+if __name__ == "__main__":
+    main()
