d596ab81794ae78da977b7b9412b1ebbed55b62e
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2020-09-17 09:50:00 +0200

    Reformat to match our guidelines

--- include/trifinger_cameras/parse_yml.h
+++ include/trifinger_cameras/parse_yml.h
@@ -1,47 +1,47 @@
 /*********************************************************************
-* Software License Agreement (BSD License)
-* 
-*  Copyright (c) 2009, Willow Garage, Inc.
-*  All rights reserved.
-* 
-*  Redistribution and use in source and binary forms, with or without
-*  modification, are permitted provided that the following conditions
-*  are met:
-* 
-*   * Redistributions of source code must retain the above copyright
-*     notice, this list of conditions and the following disclaimer.
-*   * Redistributions in binary form must reproduce the above
-*     copyright notice, this list of conditions and the following
-*     disclaimer in the documentation and/or other materials provided
-*     with the distribution.
-*   * Neither the name of the Willow Garage nor the names of its
-*     contributors may be used to endorse or promote products derived
-*     from this software without specific prior written permission.
-* 
-*  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-*  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-*  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-*  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-*  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-*  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-*  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-*  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-*  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-*  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-*  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-*  POSSIBILITY OF SUCH DAMAGE.
-*********************************************************************/
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2009, Willow Garage, Inc.
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Willow Garage nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
 
 #ifndef CAMERA_CALIBRATION_PARSERS_PARSE_YML_H
 #define CAMERA_CALIBRATION_PARSERS_PARSE_YML_H
 
-#include <string>
+#include <sensor_msgs/CameraInfo.h>
 #include <istream>
 #include <ostream>
-#include <sensor_msgs/CameraInfo.h>
+#include <string>
 
-namespace camera_calibration_parsers {
-
+namespace camera_calibration_parsers
+{
 /**
  * \brief Write calibration parameters to a file in YAML format.
  *
@@ -49,7 +49,8 @@ namespace camera_calibration_parsers {
  * \param camera_name Name of the camera
  * \param cam_info Camera parameters
  */
-bool writeCalibrationYml(std::ostream& out, const std::string& camera_name,
+bool writeCalibrationYml(std::ostream& out,
+                         const std::string& camera_name,
                          const sensor_msgs::CameraInfo& cam_info);
 
 /**
@@ -59,7 +60,9 @@ bool writeCalibrationYml(std::ostream& out, const std::string& camera_name,
  * \param[out] camera_name Name of the camera
  * \param[out] cam_info Camera parameters
  */
-bool readCalibrationYml(std::istream& in, std::string& camera_name, sensor_msgs::CameraInfo& cam_info);
+bool readCalibrationYml(std::istream& in,
+                        std::string& camera_name,
+                        sensor_msgs::CameraInfo& cam_info);
 
 /**
  * \brief Write calibration parameters to a file in YAML format.
@@ -68,7 +71,8 @@ bool readCalibrationYml(std::istream& in, std::string& camera_name, sensor_msgs:
  * \param camera_name Name of the camera
  * \param cam_info Camera parameters
  */
-bool writeCalibrationYml(const std::string& file_name, const std::string& camera_name,
+bool writeCalibrationYml(const std::string& file_name,
+                         const std::string& camera_name,
                          const sensor_msgs::CameraInfo& cam_info);
 
 /**
@@ -78,9 +82,10 @@ bool writeCalibrationYml(const std::string& file_name, const std::string& camera
  * \param[out] camera_name Name of the camera
  * \param[out] cam_info Camera parameters
  */
-bool readCalibrationYml(const std::string& file_name, std::string& camera_name,
+bool readCalibrationYml(const std::string& file_name,
+                        std::string& camera_name,
                         sensor_msgs::CameraInfo& cam_info);
 
-} //namespace camera_calibration_parsers
+}  // namespace camera_calibration_parsers
 
 #endif
