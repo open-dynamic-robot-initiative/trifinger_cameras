1cf4300b3b44148b2bf71ff77a73bf2263088caa
Felix Widmaier <felix.widmaier@tuebingen.mpg.de>
2022-03-21 15:10:51 +0100

    Add application to list connected Pylon cameras
    Also refactor the CMakeLists.txt a bit to make it easier to
    conditionally install targets and change the install destination for
    executables to lib/project_name, so they can be run with `ros2 run`.

---
+++ src/pylon_list_cameras.cpp
@@ -0,0 +1,47 @@
+/**
+ * @file
+ * @brief Application to list all detected Pylon cameras
+ */
+#include <iostream>
+
+// ignore warnings in the pylon headers
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wpedantic"
+#include <pylon/PylonIncludes.h>
+#pragma GCC diagnostic pop
+
+int main()
+{
+    int ret = 0;
+    try
+    {
+        Pylon::PylonInitialize();
+        Pylon::CTlFactory& tl_factory = Pylon::CTlFactory::GetInstance();
+        Pylon::DeviceInfoList_t device_list;
+
+        if (tl_factory.EnumerateDevices(device_list) == 0)
+        {
+            std::cout << "No cameras found." << std::endl;
+        }
+        else
+        {
+            Pylon::DeviceInfoList_t::const_iterator device_iterator;
+            int i = 1;
+            for (device_iterator = device_list.begin();
+                 device_iterator != device_list.end();
+                 ++i, ++device_iterator)
+            {
+                std::string camera_name(device_iterator->GetUserDefinedName());
+                std::cout << i << ". " << camera_name << std::endl;
+            }
+        }
+    }
+    catch (const Pylon::GenericException& e)
+    {
+        std::cerr << "Camera Error: " << e.what() << std::endl;
+        ret = 1;
+    }
+    Pylon::PylonTerminate();
+
+    return ret;
+}
