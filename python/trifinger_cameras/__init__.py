b201f43b75202048221608f78eb41a8ab03f7c49
Felix Kloss <felix.kloss@uni-tuebingen.de>
2025-03-17 13:37:30 +0100

    Add camera info in HDF5 log file
    The script to convert camera logs to HDF5 files now also requires the
    camera parameter files and uses them to add the calibration information
    to the HDF5 file.
    This makes the files more self-contained and more convenient to handle
    (no need to keep the separate calibration files around).
    
    Increase the format version to 2 and update the log viewer script
    accordingly (it can work with both versions 1 and 2).
    
    To avoid redundancy, add a tuple with camera names and the tricamera log
    magic byte to the `__init__.py` of the package.

--- python/trifinger_cameras/__init__.py
+++ python/trifinger_cameras/__init__.py
@@ -2,3 +2,10 @@
 # F401 = unused import, F403 = complaint about `import *`.
 from . import py_camera_types as camera  # noqa: F401
 from . import py_tricamera_types as tricamera  # noqa: F401
+
+
+#: Names of the TriFinger cameras in the order in which they are usually handled.
+CAMERA_NAMES = ("camera60", "camera180", "camera300")
+
+#: Magic byte used to identify TriCamera logs (currently only used in HDF5 files).
+TRICAMERA_LOG_MAGIC = 0x3CDA7A00
